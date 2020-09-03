b2efa5f51660f050d250f59f432f866a172fac59
Peter NTSUNTSHA <pntsunts@c4r11s11.wethinkcode.co.za>
2020-09-03 15:13:20 +0200

    Done

--- src/weather/Tower.java
+++ src/weather/Tower.java
@@ -8,17 +8,24 @@ import src.vehicles.Flyable;
 public abstract class Tower {
     private List<Flyable> observers = new ArrayList<>();
 
+    private boolean check =  false;
+
     public void register(Flyable flyable) {
         observers.add(flyable);
     }
 
-    public void unregister(Flyable flyable) {
+    public void unregister(Flyable flyable){
+        check = true;
         observers.remove(flyable);
     }
 
     protected void conditionsChanged() {
         int i = 0;
         while (i < observers.size()){
+            if (check == true){  
+                i--;
+                check = false;
+            }
             observers.get(i).updateConditions();
             i++;
         }
